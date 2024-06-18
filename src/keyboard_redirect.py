fb86eb30ac049e1cb8337b8aef6e77b66ac23434
Shubhanshu <shubhanshu004@gmail.com>
2024-06-18 15:55:19 +0530

    experiment shifting logic

--- src/keyboard_redirect.py
+++ src/keyboard_redirect.py
@@ -2,6 +2,6 @@ import pyautogui
 
 def redirect_input(gaze_direction, key):
     if gaze_direction == 'left':
-        pyautogui.hotkey('win', 'left')
+        pyautogui.click(x=0, y=0)
     elif gaze_direction == 'right':
-        pyautogui.hotkey('win', 'right')
+        pyautogui.click(x=-2400, y=0)
